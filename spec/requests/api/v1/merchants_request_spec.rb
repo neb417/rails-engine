81c039c04dd477fa0a8aff7caf509abc8b0b3010
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-10-17 20:15:50 -0600

    add serializer

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -6,21 +6,17 @@ RSpec.describe "Merchants API" do
   describe 'merchants index' do
     before(:each) do
       get '/api/v1/merchants'
-      # merchants = JSON.parse(response.body, symbolize_names: true)
     end
 
     it "response is successful" do
       expect(response).to be_successful
+      binding.pry
     end
 
-    it 'has correct count of merchants' do
+    it 'has correct count of merchants and attributes' do
       merchants = JSON.parse(response.body, symbolize_names: true)
 
       expect(merchants.count).to eq(10)
-    end
-
-    it 'has correct attributes' do
-      merchants = JSON.parse(response.body, symbolize_names: true)
 
       merchants.each do |merchant|
         expect(merchant).to have_key(:id)
@@ -28,6 +24,31 @@ RSpec.describe "Merchants API" do
         expect(merchant).to have_key(:name)
         expect(merchant[:name]).to be_a(String)
       end
+    end
+  end
+
+  describe 'merchant show' do
+    before(:each) do
+      get "/api/v1/merchants/#{merchant_list.first[:id]}"
+    end
+
+    it 'response is successful' do
+      expect(response).to be_successful
+    end
+
+    it 'requests only one merchant' do
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
+    end
+
+    it 'returns 4xx' do
+      get "/api/v1/merchants/#{merchant_list.last[:id] + 1}"
+
+      expect(resonse.status).to eq(404)
     end
   end
 end
