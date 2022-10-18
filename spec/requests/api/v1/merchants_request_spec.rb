cf09cba71ec97e951a6450c49fa61ec72552cfee
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-10-17 18:46:14 -0600

    add all merchants to api index

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,14 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "Merchants API" do
-  let!(:merchants) {create_list(:merchant, 3)}
-  describe 'all merchants' do
-    before :each do
+  let!(:merchant_list) {create_list(:merchant, 10)}
+
+  describe 'merchants index' do
+    before(:each) do
       get '/api/v1/merchants'
+      # merchants = JSON.parse(response.body, symbolize_names: true)
     end
 
     it "response is successful" do
       expect(response).to be_successful
+    end
+
+    it 'has correct count of merchants' do
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants.count).to eq(10)
+    end
+
+    it 'has correct attributes' do
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      merchants.each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_an(Integer)
+        expect(merchant).to have_key(:name)
+        expect(merchant[:name]).to be_a(String)
+      end
     end
   end
 end
