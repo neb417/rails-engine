a4c174db76d02abac04a1fb7ba96b1923f305e80
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-10-18 07:30:52 -0600

    render single merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,21 +8,21 @@ RSpec.describe "Merchants API" do
       get '/api/v1/merchants'
     end
 
-    it "response is successful" do
+    it 'response is successful' do
       expect(response).to be_successful
-      binding.pry
     end
 
     it 'has correct count of merchants and attributes' do
-      merchants = JSON.parse(response.body, symbolize_names: true)
-
+      merchants = JSON.parse(response.body, symbolize_names: true)[:data]
       expect(merchants.count).to eq(10)
 
       merchants.each do |merchant|
         expect(merchant).to have_key(:id)
-        expect(merchant[:id]).to be_an(Integer)
-        expect(merchant).to have_key(:name)
-        expect(merchant[:name]).to be_a(String)
+        expect(merchant[:id]).to be_a(String)
+        expect(merchant).to have_key(:attributes)
+        expect(merchant[:attributes]).to be_a(Hash)
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a(String)
       end
     end
   end
@@ -37,15 +37,17 @@ RSpec.describe "Merchants API" do
     end
 
     it 'requests only one merchant' do
-      merchant = JSON.parse(response.body, symbolize_names: true)
+      merchant = JSON.parse(response.body, symbolize_names: true)[:data]
 
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      expect(merchant[:id]).to be_an(String)
+      expect(merchant).to have_key(:attributes)
+      expect(merchant[:attributes]).to be_a(Hash)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
 
-    it 'returns 4xx' do
+    xit 'returns 4xx' do
       get "/api/v1/merchants/#{merchant_list.last[:id] + 1}"
 
       expect(resonse.status).to eq(404)
