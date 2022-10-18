394aff92faa2490caf1a6e18291efa8f01c70c41
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-10-18 13:05:21 -0600

    add tests for items api.

--- spec/requests/api/v1/merchant_items_request_spec.rb
+++ spec/requests/api/v1/merchant_items_request_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Merchant's Items API" do
-  let!(:merchant_list) {create_list(:merchant, 10)}
-  let!(:item_list) {create_list(:item, 50)}
+  let!(:merchant_list) { create_list(:merchant, 10) }
+  let!(:item_list) { create_list(:item, 50) }
 
   before(:each) do
     get api_v1_merchant_items_path(merchant_list.first)
