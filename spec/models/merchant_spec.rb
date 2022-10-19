11ebd235f0ab64206367168c2760a02f82668cfc
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-10-19 15:59:07 -0600

    add test for model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Merchant do
   describe 'validations' do
     it { should validate_presence_of :name }
   end
+
+  describe 'class methods' do
+    let!( :merchant_list ) { create_list(:merchant, 5) }
+
+    it '#find_by_name' do
+      expect(Merchant.find_by_name('e')).to be_a(Merchant)
+    end
+  end
 end
