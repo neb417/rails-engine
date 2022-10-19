0938a2c14b05fe5db6ed2da091924eccb437cf41
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-10-19 15:53:42 -0600

    add routes, controller, model for find merchant. add #find and #find_by_name.

---
+++ app/controllers/api/v1/find_merchants_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::FindMerchantsController < ApplicationController
+
+  def find
+    merchant = Merchant.find_by_name(params[:name])
+
+    if merchant.nil?
+      render json: { data: { error: 'Merchant not found' } }
+    else
+      render json: MerchantSerializer.new(merchant)
+    end
+  end
+end
--- spec/requests/api/v1/merchants/find_spec.rb
+++ spec/requests/api/v1/merchants/find_spec.rb
@@ -3,22 +3,36 @@ require 'rails_helper'
 RSpec.describe 'Non-ReSTful API' do
   let!(:merchant_list) { create_list(:merchant, 10) }
 
-  before :each do
-    get api_v1_merchant_find_path(merchant_list.first.id)
-    merchant = JSON.parse(response.body, symbolize_names: true)[:data]
-  end
-
   it 'response is succcessful' do
+    find_params = { name: 'o' }
+
+    get api_v1_merchants_find_path, params: find_params
+
     expect(response).to be_successful
     expect(response.status).to eq(200)
   end
 
   it 'returns 1 merchant with correct attributes' do
+    find_params = {name: 'o'}
+
+    get api_v1_merchants_find_path, params: find_params
+
+    merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+
     expect(merchant).to have_key(:id)
     expect(merchant[:id]).to be_an(String)
     expect(merchant).to have_key(:attributes)
     expect(merchant[:attributes]).to be_a(Hash)
     expect(merchant[:attributes]).to have_key(:name)
     expect(merchant[:attributes][:name]).to be_a(String)
+  end
+
+  it 'returns 1 merchant with correct attributes' do
+    find_params = {name: 'XZ'}
+
+    get api_v1_merchants_find_path, params: find_params
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
   end
 end
